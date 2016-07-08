c3ffa27621c26ff27e077f322ad3e66364c46556
Lorenzo Stoakes <lstoakes@gmail.com>
2016-07-08 09:45:04 +0100

    Add stack allocation and separate out allocation types.

--- experiments/simple_alloc.c
+++ experiments/simple_alloc.c
@@ -3,10 +3,30 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define PAGES      1025
-#define ADDITIONAL 69
+#define PAGES       1025
+#define ADDITIONAL  69
+#define STACK_COUNT 7000
 
-int main(void)
+static void stack_alloc_recur(int count)
+{
+	char dummy[1024];
+
+	if (count <= 0)
+		return;
+
+	dummy[0] = 'x';
+
+	stack_alloc_recur(count-1);
+
+	dummy[1023] = dummy[0];
+}
+
+static void stack_alloc(void)
+{
+	stack_alloc_recur(STACK_COUNT);
+}
+
+static void heap_alloc(void)
 {
 	long i;
 	long page_size = sysconf(_SC_PAGESIZE);
@@ -17,6 +37,12 @@ int main(void)
 		buf[i] = 'x';
 
 	free(buf);
+}
+
+int main(void)
+{
+	heap_alloc();
+	stack_alloc();
 
 	return EXIT_SUCCESS;
 }
